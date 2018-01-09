b7e997a059647eb76f5fbc4c1429ee7ca08adcaf
eiichi hayashi <eiichi@mac.com>
2018-01-09 15:35:29 +0900

    s5

--- JStest3/app.js
+++ JStest3/app.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function() {
 
   for(i=0;i < 10 ; i++)
   {
-  str = prompt(str + i + "わーい１１１１2222 <br>");
+  str = prompt(str + i + "わーい１１１１2222555 <br>");
   }
 
   confirm(str);
