168d55b4c8d588a30414b590718a7304c50d25bd
eiichi hayashi <eiichi@mac.com>
2018-01-09 14:18:22 +0900

    ｄｓｄｓ

--- JStest3/app.js
+++ JStest3/app.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", function() {
   console.log("Hello World!");
-  
+
   var str= prompt("わーい<br>");
-  
+
   for(i=0;i < 10 ; i++)
   {
   str = prompt(str + i + "わーい <br>");
-  } 
+  }
+
   confirm(str);
 });
